aca457d92950f8f9bfce7f3002cfb57cfd147db8
Trovix <james.platt@live.co.uk>
2026-04-16 14:25:49 +0100

    Setup ISR framework

--- main/main.cpp
+++ main/main.cpp
@@ -1,21 +1,50 @@
+/**
+Gear Shifter Code 
+For any questions contact James Platt
+
+The design philosophy prioritises safety. At the time of writing this software my feeling was the best way of doing 
+this was for the shifting events (i.e UP, DOWN, NEUTRAL) to have their own dedicated fully blocking ISRs.
+In these ISRs any additional interrupts are IGNORED and freertos task switching is DISABLED. 
+It effectively "locks" the autoshifter into a "critical section" of code which it will only exit if either:
+    - The "shifted" position is reached (i.e succesful shift has occured)
+    - A timeout occurs. This will be logged as an error on CAN. (but not hard fail).
+When a shift event is detected (i.e user button input) a few safety checks are done. These are written to be as fast 
+as possible without compromising on safety. Then the output PWM command to the ESC is sent. 
+
+Some of the important safety checks are: 
+    - the requested gear shift does not result in an out-of-range gear (i.e shifting into "6th")
+    - the ECU measured gear and the gear shifter's internal count "agree"
+    
+**/
+
+
+
+
+
+//Esp logging library (a nicer way of "printing to serial" it allows you to specify different levels of importance e.g LOGW (warning) LOGE (error) LOGI (info)
 #include "esp_log.h"
 
-#include "freertos/FreeRTOS.h"
+//freertos libraries for tasks and mutexes 
+#include "freertos/FreeRTOS.h"                           
 #include "freertos/semphr.h"
-#include "freertos/task.h"
+#include "freertos/task.h" 
 
+//the two tasks that are being used.
+ 
 #include "can_task.hpp"
 #include "gear_safety_task.hpp"
 
+//logging tag 
 static const char *TAG_SYS = "SYSTEM";
 
+//entry point for the program
 extern "C" void app_main(void)
 {
     ESP_LOGI(TAG_SYS, "Hello World");
     ESP_LOGI(TAG_SYS, "Creating mutexes...");
        
-    SemaphoreHandle_t gear_count_mutex = xSemaphoreCreateMutex();
-    SemaphoreHandle_t gear_ecu_mutex = xSemaphoreCreateMutex();
+    SemaphoreHandle_t gear_count_mutex = xSemaphoreCreateMutex(); 
+    SemaphoreHandle_t gear_ecu_mutex = xSemaphoreCreateMutex();   
 
     if((gear_count_mutex == NULL) || gear_ecu_mutex == NULL){
         ESP_LOGE(TAG_SYS, "Mutex creation failed!");
